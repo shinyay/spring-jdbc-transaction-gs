96b62ff70329303f627e61eb582c3a759fcbbe39
shinyay <shinya.com@gmail.com>
2020-10-17 23:17:01 +0900

    Add annotation for Transaction

--- src/main/kotlin/dom/google/shinyay/service/BookingService.kt
+++ src/main/kotlin/dom/google/shinyay/service/BookingService.kt
@@ -3,10 +3,12 @@ package dom.google.shinyay.service
 import dom.google.shinyay.logger
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class BookingService(val jdbcTemplate: JdbcTemplate) {
 
+    @Transactional
     fun book(vararg persons: String) {
         for (person in persons) {
             logger.info("Booking $person in a seat...")
