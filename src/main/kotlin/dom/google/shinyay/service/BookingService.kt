9963703d3fc204966c9be76d949eb1d2c78b597f
shinyay <shinya.com@gmail.com>
2020-10-17 23:16:01 +0900

    Add booking function for multiple people

--- src/main/kotlin/dom/google/shinyay/service/BookingService.kt
+++ src/main/kotlin/dom/google/shinyay/service/BookingService.kt
@@ -1,8 +1,16 @@
 package dom.google.shinyay.service
 
+import dom.google.shinyay.logger
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
 
 @Service
 class BookingService(val jdbcTemplate: JdbcTemplate) {
+
+    fun book(vararg persons: String) {
+        for (person in persons) {
+            logger.info("Booking $person in a seat...")
+            jdbcTemplate.update("insert into BOOKINGS(FIRST_NAME) values (?)", person)
+        }
+    }
 }
