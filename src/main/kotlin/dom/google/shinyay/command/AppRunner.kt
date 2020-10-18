a6519e4d8b61e56b9981792cbd50395b6d2def5b
shinyay <shinya.com@gmail.com>
2020-10-18 21:43:11 +0900

    Add Commandline class

---
+++ src/main/kotlin/dom/google/shinyay/command/AppRunner.kt
@@ -0,0 +1,11 @@
+package dom.google.shinyay.command
+
+import org.springframework.boot.CommandLineRunner
+import org.springframework.stereotype.Component
+
+@Component
+class AppRunner : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        TODO("Not yet implemented")
+    }
+}
