557a1f649c8121c4fd96d5f0a18c1d50414a548c
shinyay <shinya.com@gmail.com>
2020-10-18 22:05:51 +0900

    Add insert data successful case and abnormal case

--- src/main/kotlin/dom/google/shinyay/command/AppRunner.kt
+++ src/main/kotlin/dom/google/shinyay/command/AppRunner.kt
@@ -1,12 +1,23 @@
 package dom.google.shinyay.command
 
+import dom.google.shinyay.logger
 import dom.google.shinyay.service.BookingService
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
+import org.springframework.util.Assert
 
 @Component
 class AppRunner(val bookingService: BookingService) : CommandLineRunner {
     override fun run(vararg args: String?) {
-        TODO("Not yet implemented")
+        bookingService.book("Alice", "Bob", "Carol")
+        Assert.isTrue(bookingService.showAllBookings()!!.size == 3,
+                "First booking should work with no problem")
+        logger.info("Alice, Bob and Carol have been booked")
+        try {
+            bookingService.book("Chris", "Charlie")
+        } catch (e: RuntimeException) {
+            logger.info("[The following exception is expect the person is too big for the DB]")
+            logger.error(e.message)
+        }
     }
 }
