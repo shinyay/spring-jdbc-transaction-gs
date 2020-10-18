74ed96961b4523cef5e0a5efeae6ff3278706140
shinyay <shinya.com@gmail.com>
2020-10-18 21:44:45 +0900

    Add BookingService in Constructor

--- src/main/kotlin/dom/google/shinyay/command/AppRunner.kt
+++ src/main/kotlin/dom/google/shinyay/command/AppRunner.kt
@@ -1,10 +1,11 @@
 package dom.google.shinyay.command
 
+import dom.google.shinyay.service.BookingService
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
 
 @Component
-class AppRunner : CommandLineRunner {
+class AppRunner(val bookingService: BookingService) : CommandLineRunner {
     override fun run(vararg args: String?) {
         TODO("Not yet implemented")
     }
